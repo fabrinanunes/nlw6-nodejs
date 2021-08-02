cbbf2b4a9e5790cc5957789ae77b8c61ed4179ef
Fabrina <fabrina.nunes@hotmail.com>
2021-08-02 18:35:37 -0300

    ajustes aula 01

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
 import express, { request, response } from "express"
+import "reflect-metadata"
 
 const app = express()
-
-app.get("/test", (request, response) =>{
-    return response.send('Olá NLW')
-})
-
-app.post('/test-post', (request, response) => {
-    return response.send('Olá NLW método POST')
-})
 
 app.listen(3000, () => console.log('Server is running'))
